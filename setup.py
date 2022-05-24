314316e16cd99af6697054ae902bcdf54aed0d59
hfbraga <herigson@hexagon.com>
2022-05-23 23:39:18 -0300

    setup to publish api

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='quality-control',
+    version='',
+    packages=['app', 'app.models', 'app.controllers'],
+    url='',
+    license='',
+    author='hfbraga',
+    author_email='',
+    description=''
+)
