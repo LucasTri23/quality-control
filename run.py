ba27f5aacd7fb486c62e5d3f6a59b676290b26cc
herigson <felipevbraga@outlook.com>
2022-05-25 10:54:26 -0300

    Revert "procfile 0.0.2"
    This reverts commit 766c6511794e163a8c34190f3a8b4b04381e5e77.

--- run.py
+++ run.py
@@ -2,6 +2,5 @@ from app import app
 local_host = '0.0.0.0'
 #private ip aws instance
 remote_host = '172.31.24.18'
-
 if __name__ == '__main__':
         app.run(host=local_host, port=443, debug=True)
