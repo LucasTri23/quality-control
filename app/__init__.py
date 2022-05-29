a7e5834d1c1c3297de3b5a3b48f94ffd52f8ee74
herigson <felipevbraga@outlook.com>
2022-05-29 03:08:16 -0300

    added config to deploy app

--- app/__init__.py
+++ app/__init__.py
@@ -10,27 +10,35 @@ app = Flask(__name__)
 app.config.from_object('config.ProductionConfig')
 app.config['JSON_AS_ASCII'] = False
 app.config['SECRET_KEY'] = 'secret'
-db = SQLAlchemy(app)
-ma = Marshmallow(app)
-CORS(app)
-jwt = JWTManager(app)
+db = SQLAlchemy()
+ma = Marshmallow()
+jwt = JWTManager()
 
-#import models
-from app.models import device
-from app.models import user
-from app.models import employee
 
-#implement logic to see if the bank already exists
-db.create_all()
-# #import controllers
-from app.controllers.device_controller import DeviceController
-from app.controllers.user_controller import UserController
-from app.controllers.test_controller import TestController
-from app.controllers.result_controller import ResultController
-from app.controllers.auth_controller import AuthController
+def create_app():
+    db.init_app(app)
+    ma.init_app(app)
+    jwt.init_app(app)
 
-app.register_blueprint(DeviceController.device_controller, url_prefix="/api/v1")
-app.register_blueprint(UserController.user_controller, url_prefix="/api/v1")
-app.register_blueprint(TestController.test_controller, url_prefix="/api/v1")
-app.register_blueprint(ResultController.result_controller, url_prefix="/api/v1")
-app.register_blueprint(AuthController.auth_controller, url_prefix="/api/v1")
+    #import models
+    from app.models import device
+    from app.models import user
+    from app.models import employee
+
+    #implement logic to see if the bank already exists
+    with app.app_context():
+        db.create_all()
+    # #import controllers
+    from app.controllers.device_controller import DeviceController
+    from app.controllers.user_controller import UserController
+    from app.controllers.test_controller import TestController
+    from app.controllers.result_controller import ResultController
+    from app.controllers.auth_controller import AuthController
+
+    app.register_blueprint(DeviceController.device_controller, url_prefix="/api/v1")
+    app.register_blueprint(UserController.user_controller, url_prefix="/api/v1")
+    app.register_blueprint(TestController.test_controller, url_prefix="/api/v1")
+    app.register_blueprint(ResultController.result_controller, url_prefix="/api/v1")
+    app.register_blueprint(AuthController.auth_controller, url_prefix="/api/v1")
+
+    return app
--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-from app import app
+from app import create_app
 local_host = '0.0.0.0'
 #private ip aws instance
 remote_host = '172.31.24.18'
+app = create_app()
 if __name__ == '__main__':
         # app.run(host=local_host, port=8080, debug=True)
         app.run()
